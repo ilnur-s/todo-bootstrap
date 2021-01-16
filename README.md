98907d45929fbcc4d01a13a9dcb1eba691803bd8
ilnur-s <sadneur@gmail.com>
2021-01-16 16:39:02 +0300

    refactoring

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # To Do List
-Небольшое To Do приложение с использованием стилей Bootstrap. Попытка реализовать архитектурный паттерн MVC на ванильном JavaScript с использованием библиотеки [on-change](https://github.com/sindresorhus/on-change "on-change"). Также был использован сборщик Webpack с самописным файлом конфигурации.
-
-Это первая версия. Проект дорабатывается.
+Небольшое To Do приложение с использованием стилей Bootstrap. Попытка реализовать архитектурный паттерн MVC на ванильном JavaScript с использованием библиотеки [on-change](https://github.com/sindresorhus/on-change "on-change"). Также был использован сборщик Webpack.
 
 ## Установка
 `npm run install`
--- src/index.js
+++ src/index.js
@@ -5,48 +5,79 @@ import '../css/style.css';
 const addButton = document.querySelector('.add-task');
 const selectAllButton = document.querySelector('.select-all');
 const deleteButton = document.querySelector('.delete-selection');
+const list = document.querySelector('.list-group');
+const inputArea = document.querySelector('.form-control');
 
 const data = [];
 
 const watchedState = onChange(
   data,
-  (__path, state) => {
-    if (document.querySelectorAll('li').length < data.length) {
-      const olElement = document.querySelector('ol');
-      const liElement = document.createElement('li');
-      liElement.textContent = data[data.length - 1].value;
-      olElement.append(liElement);
+  (__path, passedValue) => {
+    const listGroup = document.querySelectorAll('.list-group-item');
+
+    if (listGroup.length < data.length) {
+      const listElement = document.createElement('li');
+      const inputElement = document.createElement('input');
+
+      inputElement.type = 'checkbox';
+      inputElement.classList.add('form-check-input', 'me-2');
+
+      listElement.textContent = data[data.length - 1].value;
+      listElement.classList.add('list-group-item', 'list-group-item-action');
+
+      listElement.prepend(inputElement);
+      list.append(listElement);
+
       document.querySelector('.form-control').value = '';
     }
 
-    if (state === true) {
+    if (passedValue === true || passedValue === false) {
       data.forEach((task, index) => {
-        const allTasks = document.querySelectorAll('li');
         if (task.done) {
-          allTasks[index].classList.add('done');
+          listGroup[index].classList.add('done');
+          listGroup[index].firstChild.checked = true;
         } else {
-          allTasks[index].classList.remove('done');
+          listGroup[index].classList.remove('done');
+          listGroup[index].firstChild.checked = false;
         }
       });
     }
 
-    if (document.querySelectorAll('li').length > data.length) {
-      const olElement = document.querySelector('ol');
-      while (olElement.firstChild) {
-        olElement.removeChild(olElement.firstChild);
+    if (listGroup.length > data.length) {
+      while (list.firstChild) {
+        list.removeChild(list.firstChild);
       }
       data.forEach((task) => {
-        const liElement = document.createElement('li');
-        liElement.textContent = task.value;
-        olElement.append(liElement);
+        const listElement = document.createElement('li');
+        const inputElement = document.createElement('input');
+
+        inputElement.type = 'checkbox';
+        inputElement.classList.add('form-check-input', 'me-2');
+
+        listElement.textContent = task.value;
+        listElement.classList.add('list-group-item', 'list-group-item-action');
+
+        listElement.prepend(inputElement);
+        list.append(listElement);
       });
     }
   },
 );
 
 addButton.addEventListener('click', () => {
-  const inputValue = document.querySelector('.form-control').value;
+  const inputValue = inputArea.value;
   if (inputValue) {
+    const task = {
+      value: inputValue,
+      done: false,
+    };
+    watchedState.push(task);
+  }
+});
+
+inputArea.addEventListener('keyup', (event) => {
+  const inputValue = inputArea.value;
+  if (event.keyCode === 13 && inputValue) {
     const task = {
       value: inputValue,
       done: false,
@@ -63,5 +94,16 @@ selectAllButton.addEventListener('click', () => {
 
 deleteButton.addEventListener('click', () => {
   const newData = data.filter((task) => !task.done);
-  watchedState.splice(0, ...newData);
+  watchedState.splice(0, data.length, ...newData);
+});
+
+list.addEventListener('click', (event) => {
+  const targetElement = event.target;
+  const indexListElement = Array.from(document.querySelectorAll('.list-group-item')).indexOf(targetElement);
+  if (indexListElement !== -1) {
+    watchedState[indexListElement].done = !data[indexListElement].done;
+  } else {
+    const indexInputElement = Array.from(document.querySelectorAll('.form-check-input')).indexOf(targetElement);
+    watchedState[indexInputElement].done = !data[indexInputElement].done;
+  }
 });
